b0ee22cb2187c25faba441fbaaac9d3ec4e550c2
Ryuto Matsuoka-yi <ryuto@192-168-1-130.tpgi.com.au>
2020-11-13 18:03:52 +1100

    progress

--- JavaScript.js
+++ JavaScript.js
@@ -4,3 +4,82 @@ const calculator = {
     waitingForSecondOperand: false,
     operator: null
 };
+
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+    
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        // Overwrite 'displayValue' if the current value is 0, otherwise append to it
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+    
+    console.log(calculator);
+}
+
+function inputDecimal(dot) {
+    // If the 'displayValue' property does not contain a decimal point
+    if ( !calculator.displayValue.includes(dot) ) {
+        // Append the decimal point
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator;
+    // Convert the string contents of 'displayValue' to a floating point number
+    const inputValue = parseFloat(displayValue);
+
+    // Verify that 'firstOperand' is null and that 'inputValue' is not a NaN value
+    if ( firstOperand === null && !isNaN(inputValue) ) {
+        // Update the firstOperand property
+        calculator.firstOperand = inputValue;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+}
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    //update the value of the calculator screen with the contents of 'displayValue' property
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    // Access the clicked element
+    // Same as const target = event.target;
+    const { target } = event;
+
+    //Check if the clicked element is a button. If it's not, then exit function
+    if ( !target.matches('button') ) {
+        return;
+    }
+
+    if ( target.classList.contains('operator') ) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if ( target.classList.contains('decimal') ) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if ( target.classList.contains('all-clear') ) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
